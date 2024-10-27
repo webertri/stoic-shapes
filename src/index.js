5143e9034d53bf86359575bbc579a7885ffef4d5
Colton Hurst <colton@coltonhurst.com>
2024-10-26 21:45:06 -0400

    Set up color theme and main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+drawRectangle(20, 20, 20, 20, "blue");
+
+function drawRectangle(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(
+        Math.round(x),
+        Math.round(y),
+        Math.round(width),
+        Math.round(height)
+    );
+}
