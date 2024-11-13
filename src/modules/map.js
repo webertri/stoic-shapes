6a8f339797298347775e938bf4394d70e0393a3c
Colton Hurst <colton@coltonhurst.com>
2024-11-13 01:28:54 -0500

    Add map loading functionality

--- src/modules/map.js
+++ src/modules/map.js
@@ -1,3 +1,5 @@
+import { Wall } from './wall.js'
+
 /*
     The class representing a map.
 
@@ -9,6 +11,60 @@ class Map {
         this.id = id;
         this.name = name;
         this.entities = [];
+    }
+
+    /*
+        A private function used during load()
+
+        Will create an entity and "spawn" it based
+        on the key in mapData.
+    */
+    #spawnDuringLoad(id, row, column, key) {
+        const WALL_SIZE = 20;
+        const WALL_COLOR = "black";
+
+        switch (key) {
+            case "w":
+                this.spawn(new Wall(id, "wall", column * 20, row * 20, WALL_SIZE, WALL_COLOR));
+                console.log("Spawned a wall at: (" + row + ", " + column + ")");
+                break;
+        }
+    }
+
+    /*
+        Takes a 2D array representing the map & spawns the
+        entities in accordingly.
+
+        Returns true if the load succeeded, false if not.
+
+        - mapData should be 20 rows x 35 columns
+        - entity representations in the array:
+            - 'w' = wall
+    */
+    load(mapData) {
+        // Verify mapData is an array and there are 20 rows
+        if (!Array.isArray(mapData) || mapData.length != 20) {
+            console.error("Error during load, mapData is not an array OR there are not 35 columns.");
+            console.log("mapData is an array: " + Array.isArray(mapData));
+            console.log("mapData has " + mapData.length + " rows.");
+            return false;
+        }
+
+        // Verify each row in mapData has 35 columns
+        for (let i = 0; i < mapData.length; i++) {
+            if (mapData[i].length != 35) {
+                return false;
+            }
+        }
+
+        // Spawn each entity based on entry in mapData
+        let id = 1;
+        for (let i = 0; i < mapData.length; i++) {
+            for (let j = 0; j < mapData[i].length; j++) {
+                this.#spawnDuringLoad(id, i, j, mapData[i][j]);
+                id++;
+            }
+        }
     }
 
     /*
